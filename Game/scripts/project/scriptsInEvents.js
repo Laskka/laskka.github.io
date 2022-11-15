ee74f4f6ed933cf4ce76e42eb28613f2166e601f
Laskka <laskkains@gmail.com>
2022-11-16 01:23:11 +0800

    chack return value from constract 3

---
+++ Game/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async списоксобытий1_Event1_Act1(runtime, localVars)
+	{
+		Show(runtime.globalVars.TargetValue);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
--- conector.js
+++ conector.js
@@ -1,4 +1,4 @@
-class Conector
+function Show(value) 
 {
-    
+    alert(value);
 }
